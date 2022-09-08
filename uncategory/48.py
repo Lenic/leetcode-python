f0d3bfd53497def9a26241c088e70c705f8d37f2
Lenic <Lenic@live.cn>
2022-09-08 21:58:43 +0800

    使用双重循环解决题目：48
    Signed-off-by: Lenic <Lenic@live.cn>

---
+++ uncategory/48.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        max = n - 1
+        for i in range(n >> 1):
+            for j in range(i, n - i - 1):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[max - j][i]
+                matrix[max - j][i] = matrix[max - i][max - j]
+                matrix[max - i][max - j] = matrix[j][max - i]
+                matrix[j][max - i] = tmp
+
+
+m1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Solution().rotate(m1)
+print(m1)
