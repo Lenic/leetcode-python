cdf033a98b1b195096c07b4f40c5cd74bdee0d8f
Lenic <Lenic@live.cn>
2022-09-13 12:46:34 +0800

    使用单纯的回溯解决题目：491
    - 通过 set 的方式解决重复元素问题
    - 不能通过前一个元素的方式判断，因为有可能存在中断区间
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- back-tracking/491.py
+++ back-tracking/491.py
@@ -5,45 +5,28 @@ class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         res: List[List[int]] = []
 
-        def traverslSegment(begin: int, end: int):
-            n = end - begin
-            visited = [False] * n
+        def dfs(ans: List[int], index: int):
+            if len(ans) > 1:
+                res.append(ans[:])
 
-            def dfs(ans: List[int], i: int):
-                if i == end:
-                    if len(ans) > 1:
-                        res.append(ans[:])
-                    return
+            cache = set()
+            for i in range(index, len(nums)):
+                item = nums[i]
 
-                visitedIndex = i - left
-                if i > begin and nums[i] == nums[i - 1] and (not visited[visitedIndex - 1]):
-                    dfs(ans, i + 1)
-                    return
+                if (len(ans) and item < ans[-1]) or (item in cache):
+                    continue
+                ans.append(item)
 
-                visited[visitedIndex] = True
-                ans.append(nums[i])
+                cache.add(item)
                 dfs(ans, i + 1)
-                visited[visitedIndex] = False
                 ans.pop()
-                dfs(ans, i + 1)
 
-            dfs([], begin)
-
-        left: int = 0
-        right: int = 1
-        while right < len(nums):
-            while right < len(nums) and nums[right] >= nums[right - 1]:
-                right += 1
-            if right - left > 1:
-                traverslSegment(left, right)
-            left = right
-            right += 1
-
+        dfs([], 0)
         return res
 
 
 # too many
-print(Solution().findSubsequences([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 1, 1, 1]))
+# print(Solution().findSubsequences([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 1, 1, 1]))
 
 # [[4,4]]
 print(Solution().findSubsequences([4, 4, 3, 2, 1]))
