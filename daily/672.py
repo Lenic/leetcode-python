26f9f0c6be138fbaa453cde8723fe43125872b75
Lenic <Lenic@live.cn>
2022-09-15 19:56:41 +0800

    使用数学解完成题目：672
    Signed-off-by: Lenic <Lenic@live.cn>

---
+++ daily/672.py
@@ -0,0 +1,34 @@
+from typing import List, MutableSet
+
+
+class Solution:
+    def flipLights(self, n: int, presses: int) -> int:
+        if presses == 0:
+            return 1
+
+        if n == 0:
+            return 1
+        if n == 1:
+            return 2
+        if n == 2:
+            return 3 if presses == 1 else 4
+
+        if presses == 1:
+            return 4
+        elif presses == 2:
+            return 7
+        else:
+            return 8
+
+
+# 2
+print(Solution().flipLights(1, 1))
+
+# 3
+print(Solution().flipLights(2, 1))
+
+# 4
+print(Solution().flipLights(3, 1))
+
+# 8
+print(Solution().flipLights(4, 100))
