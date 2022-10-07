c75b5eb4c62f64480c97d68096d92d87a901a7c7
Lenic <Lenic@live.cn>
2022-10-07 22:16:15 +0800

    使用二分完成题目：744
    Signed-off-by: Lenic <Lenic@live.cn>

---
+++ binary-search/744.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left, right = 0, len(letters) - 1
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            if target >= letters[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return letters[left] if left < len(letters) else letters[0]
+
+
+# f
+print(Solution().nextGreatestLetter(["c", "f", "j"], "c"))
+
+# a
+print(Solution().nextGreatestLetter(["a", "b"], "z"))
+
+# c
+print(Solution().nextGreatestLetter(["c", "f", "j"], "a"))
