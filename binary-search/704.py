40be84d9d8f96a17c580c2b5045afe5ebd4b3878
Lenic <Lenic@live.cn>
2022-10-07 11:51:50 +0800

    使用 For 循环形式替代递归的二分查找完成题目：704
    Signed-off-by: Lenic <Lenic@live.cn>

--- binary-search/704.py
+++ binary-search/704.py
@@ -2,20 +2,17 @@ from typing import List
 
 
 class Solution:
-    def binarySearch(self, nums: List[int], target: int, left: int, right: int) -> int:
-        if left > right:
-            return -1
-
-        mid = left + ((right - left) >> 1)
-        if nums[mid] > target:
-            return self.binarySearch(nums, target, left, mid - 1)
-        elif nums[mid] < target:
-            return self.binarySearch(nums, target, mid + 1, right)
-        else:
-            return mid
-
     def search(self, nums: List[int], target: int) -> int:
-        return self.binarySearch(nums, target, 0, len(nums) - 1)
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        return -1
 
 
 # 0
