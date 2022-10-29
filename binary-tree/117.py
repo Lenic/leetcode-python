b76cd7f9a0ae2e53f07ff59b2a2e802329dc77f2
Lenic <Lenic@live.cn>
2022-10-29 23:05:28 +0800

    使用完全指针复用的方式解决题目：117
    Signed-off-by: Lenic <Lenic@live.cn>

--- binary-tree/117.py
+++ binary-tree/117.py
@@ -1,8 +1,6 @@
 from typing import Optional, List
 from typing_extensions import Self
 
-from collections import deque
-
 
 class Node:
     def __init__(self, val: int = 0, left: Self | None = None, right: Self | None = None, next: Self | None = None):
@@ -14,22 +12,31 @@ class Node:
 
 class Solution:
     def connect(self, root: Optional[Node]) -> Optional[Node]:
-        q = deque([root])
-        prev: Node | None = None
-        while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-                if node is None:
-                    continue
-                if prev is not None:
-                    prev.next = node
-                if node.left:
-                    node.left.next = node.right
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                prev = node
-            prev = None
+        cur, prev, next = root, None, None
+        while cur:
+            # 处理前一个元素的 next 指针
+            if prev is not None:
+                link = cur.left or cur.right
+                if link:
+                    prevLink = prev.right or prev.left
+                    if prevLink:
+                        prevLink.next = link
+                    prev = cur
+            # 更新前一个元素的引用
+            else:
+                prev = cur
+            # 关联当前节点的左子树的 next 到右子树
+            if cur.left:
+                cur.left.next = cur.right
+            # 更新下一行的行首元素指针
+            if next is None:
+                next = cur.left or cur.right
+            # 切换到当前行下一个元素继续
+            if cur.next:
+                cur = cur.next
+            # 当前行最后一个元素时的处理逻辑
+            else:
+                prev, cur, next = None, next, None
         return root
 
 
@@ -42,6 +49,8 @@ def polyfill(root: List[int | None]):
                 continue
             children: List[Node | None] = [None, None]
             for i in range(2):
+                if index + i >= len(root):
+                    break
                 val = root[index + i]
                 if val is not None:
                     children[i] = Node(val)
@@ -57,7 +66,7 @@ def polyfill(root: List[int | None]):
         while node:
             ans.append(node.val)
             if nextNode is None:
-                nextNode = node.left
+                nextNode = node.left or node.right
             node = node.next
         res.extend(ans)
         res.append("#")
@@ -65,8 +74,14 @@ def polyfill(root: List[int | None]):
     print(res)
 
 
-# []
-polyfill(root=[])
+# [7,#,-10,2,#,-4,3,-8,#,-1,11,#]
+polyfill(root=[7, -10, 2, -4, 3, -8, None, None, None, None, -1, 11])
+
+# [1,#,2,3,#,4,5,6,#,7,8,#]
+polyfill(root=[1, 2, 3, 4, 5, None, 6, 7, None, None, None, None, 8])
 
 # [1,#,2,3,#,4,5,7,#]
 polyfill(root=[1, 2, 3, 4, 5, None, 7])
+
+# []
+polyfill(root=[])
