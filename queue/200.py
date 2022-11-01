52380e4fa053bfb23c9c8c10eca6780061ee199f
Lenic <Lenic@live.cn>
2022-11-02 00:26:26 +0800

    使用直接修改原数组的 BFS 方式解决题目：200
    Signed-off-by: Lenic <Lenic@live.cn>

--- queue/200.py
+++ queue/200.py
@@ -5,54 +5,20 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols, count = len(grid), len(grid[0]), 0
-        visited = [[False] * cols for _ in range(rows)]
-
-        targetValue: str = "1"
-        cur: deque[tuple[int, int]] = deque([(0, 0)])
-        next: deque[tuple[int, int, str]] = deque([])
-
-        while True:
-            isChanged: bool = False
-            while cur:
-                nextTargetValue = "0" if targetValue == "1" else "1"
-                for _ in range(len(cur)):
-                    row, col = cur.popleft()
-                    if visited[row][col]:
-                        continue
-                    if grid[row][col] == targetValue:
-                        isChanged = visited[row][col] = True
-                    else:
-                        next.append((row, col, nextTargetValue))
-                        continue
-                    if row - 1 >= 0 and not visited[row - 1][col]:
-                        if grid[row - 1][col] == targetValue:
-                            cur.append((row - 1, col))
-                        else:
-                            next.append((row - 1, col, nextTargetValue))
-                    if row + 1 < rows and not visited[row + 1][col]:
-                        if grid[row + 1][col] == targetValue:
-                            cur.append((row + 1, col))
-                        else:
-                            next.append((row + 1, col, nextTargetValue))
-                    if col - 1 >= 0 and not visited[row][col - 1]:
-                        if grid[row][col - 1] == targetValue:
-                            cur.append((row, col - 1))
-                        else:
-                            next.append((row, col - 1, nextTargetValue))
-                    if col + 1 < cols and not visited[row][col + 1]:
-                        if grid[row][col + 1] == targetValue:
-                            cur.append((row, col + 1))
-                        else:
-                            next.append((row, col + 1, nextTargetValue))
-            if targetValue == "1" and isChanged:
-                count += 1
-            if next:
-                (c, r, targetValue) = next.popleft()
-                cur.append((c, r))
-            else:
-                break
-        return count
+        gr, gc, res = len(grid), len(grid[0]), 0
+        for r in range(gr):
+            for c in range(gc):
+                if grid[r][c] == "1":
+                    res += 1
+                    grid[r][c] = "0"
+                    neighbors = deque([(r, c)])
+                    while neighbors:
+                        row, col = neighbors.popleft()
+                        for x, y in [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]:
+                            if 0 <= x < gr and 0 <= y < gc and grid[x][y] == "1":
+                                grid[x][y] = "0"
+                                neighbors.append((x, y))
+        return res
 
 
 # 4
