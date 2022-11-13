720ad1d6caa2b9a6a56eb89866a7d2a56f2a6de5
雷学伟 <lenic@Lenics-MacBook-Pro.local>
2022-11-14 00:08:40 +0800

    采用相对路径导入的方式解决 stack 调用 binaryTree 的问题
    - 同步变更 binary-tree 为 binaryTree: 使之符合 Python 命名规范
    
    Signed-off-by: 雷学伟 <lenic@Lenics-MacBook-Pro.local>

--- stack/94.py
+++ stack/94.py
@@ -1,16 +1,11 @@
 from typing import List, Optional
-from typing_extensions import Self
 
+from sys import path
 from collections import deque
 
-# '../binary-tree/utils.py
+path.append("../binaryTree")
 
-
-class TreeNode:
-    def __init__(self, val: int = 0, left: Self | None = None, right: Self | None = None):
-        self.val = val
-        self.left = left
-        self.right = right
+from utils import convertArray, TreeNode
 
 
 class Solution:
@@ -36,35 +31,6 @@ class Solution:
                 q.append(item.right)
 
         return ans
-
-
-def convertArray(data: List[int | None]) -> Optional[TreeNode]:
-    if not data:
-        return None
-    item = data[0]
-    if item is None:
-        return None
-
-    root = TreeNode(item)
-    q: deque[TreeNode | None] = deque([])
-    q.append(root)
-
-    i, n = 1, len(data)
-    while q and i < n:
-        item = q.popleft()
-        if item is not None:
-            if i < n:
-                leftValue = data[i]
-                if leftValue is not None:
-                    item.left = TreeNode(leftValue)
-                    q.append(item.left)
-            if i + 1 < n:
-                rightValue = data[i + 1]
-                if rightValue is not None:
-                    item.right = TreeNode(rightValue)
-                    q.append(item.right)
-            i += 2
-    return root
 
 
 def polyfill(root: List[int | None]):
