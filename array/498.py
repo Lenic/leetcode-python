8b4c748d6704695c2a156e6e74d9ae5668d59967
Lenic Rayleigh <Lenic@live.cn>
2022-11-26 20:55:07 +0800

    缩减 if-else 的代码：498
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- array/498.py
+++ array/498.py
@@ -3,36 +3,22 @@ from typing import Generator, List
 
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        ans: List[int] = []
         m, n = len(mat), len(mat[0])
-        total, forward, mmax, nmax = m * n, True, m - 1, n - 1
+        total, forward = m * n, True
 
         def getNext(i: int, j: int) -> tuple[int, int]:
-            isEdge = False
             nonlocal forward
-            if forward:
-                isEdge = i == 0 or j == nmax
-            else:
-                isEdge = j == 0 or i == mmax
-            if not isEdge:
+            if not ((i == 0 or j == n - 1) if forward else (j == 0 or i == m - 1)):
                 return (i - 1, j + 1) if forward else (i + 1, j - 1)
             else:
-                # 首先将方向反向，所以后面的判断是相反的
                 forward = not forward
-                # 因为方向，所以此处 i 肯定是等于 0 的。接下来只需要判断 j 的值即可，如果 i != 0，那么就肯定是最后一个元素
                 if not forward:
-                    if j < nmax:
-                        return (i, j + 1)
-                    else:
-                        return (i + 1, j)
-                # 区分 i 是否等于 mmax
+                    return (i, j + 1) if j < n - 1 else (i + 1, j)
                 else:
-                    if i != mmax:
-                        return (i + 1, j)
-                    else:
-                        return (i, j + 1)
+                    return (i + 1, j) if i != m - 1 else (i, j + 1)
 
         i = j = 0
+        ans: List[int] = []
         while True:
             ans.append(mat[i][j])
             if len(ans) == total:
