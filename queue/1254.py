5ef3d250fec0a78fdedcc94f9465174b351731af
Lenic Rayleigh <Lenic@live.cn>
2022-11-28 00:14:01 +0800

    添加 BFS 的边界剪枝处理：1254
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- queue/1254.py
+++ queue/1254.py
@@ -6,6 +6,8 @@ from collections import deque
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         ans, m, n = 0, len(grid), len(grid[0])
+        if m <= 2 or n <= 2:
+            return 0
 
         def getNeighbors(x: int, y: int) -> Generator[tuple[int, int], None, None]:
             if x - 1 >= 0 and grid[x - 1][y] == 0:
@@ -17,20 +19,19 @@ class Solution:
             if y + 1 < n and grid[x][y + 1] == 0:
                 yield (x, y + 1)
 
-        for i in range(m):
-            for j in range(n):
+        for i in range(1, m - 1):
+            for j in range(1, n - 1):
                 val = grid[i][j]
                 if val == 1:
                     continue
                 isIsland, q = True, deque([(i, j)])
                 while q:
-                    for _ in range(len(q)):
-                        x, y = q.popleft()
-                        grid[x][y] = 1
-                        isEdge = x == 0 or y == 0 or x == m - 1 or y == n - 1
-                        if isEdge:
-                            isIsland = False
-                        q.extend(getNeighbors(x, y))
+                    x, y = q.popleft()
+                    grid[x][y] = 1
+                    isEdge = x == 0 or y == 0 or x == m - 1 or y == n - 1
+                    if isEdge:
+                        isIsland = False
+                    q.extend(getNeighbors(x, y))
                 if isIsland:
                     ans += 1
         return ans
