07db2aba7fd2721d8d704ee642fbbee77a84846b
Lenic Rayleigh <Lenic@live.cn>
2022-12-06 20:34:28 +0800

    使用滚动数组完成题目：118
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

---
+++ array/118.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans: List[List[int]] = [[1]]
+        cur = ans[0]
+        for _ in range(numRows - 1):
+            next = [1]
+            for i in range(1, len(cur)):
+                next.append(cur[i - 1] + cur[i])
+            next.append(1)
+            ans.append(next)
+            cur = next
+        return ans
+
+
+# [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+print(Solution().generate(5))
