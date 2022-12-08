d78237d6eab34618953826ddfe802207d6b8c34b
Lenic Rayleigh <Lenic@live.cn>
2022-12-08 23:21:38 +0800

    使用系统方法作弊完成题目：557
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- string/557.py
+++ string/557.py
@@ -1,16 +1,9 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        data, n, right = list(s), len(s), 0
-        while right < n:
-            left = right
-            while right < n and s[right] != " ":
-                right += 1
-            i = right - 1
-            while left < i:
-                data[left], data[i] = data[i], data[left]
-                left, i = left + 1, i - 1
-            right += 1
-        return "".join(data)
+        data = s.split(" ")
+        for i in range(len(data)):
+            data[i] = data[i][::-1]
+        return " ".join(data)
 
 
 # "doG gniD"
