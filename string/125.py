b07d780eb76e372ed914cf6bf85f65ff285f5628
Lenic Rayleigh <Lenic@live.cn>
2022-12-16 22:54:15 +0800

    使用字符串对撞指针完成题目：125
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- string/125.py
+++ string/125.py
@@ -1,24 +1,51 @@
-from typing import List
-
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        data: List[str] = []
         ca, cz, cA, cZ, c0, c9 = ord("a"), ord("z"), ord("A"), ord("Z"), ord("0"), ord("9")
-        for i in range(len(s)):
-            val = ord(s[i])
-            if ca <= val <= cz or c0 <= val <= c9:
-                data.append(s[i])
-            elif cA <= val <= cZ:
-                data.append(chr(val + 32))
-        i, j = 0, len(data) - 1
+        i, j = 0, len(s) - 1
+
         while i < j:
-            if data[i] != data[j]:
+            left = -1
+            while i < j:
+                val = ord(s[i])
+                if ca <= val <= cz or c0 <= val <= c9:
+                    left = val
+                    break
+                elif cA <= val <= cZ:
+                    left = val + 32
+                    break
+                i += 1
+
+            right = -1
+            while i < j:
+                val = ord(s[j])
+                if ca <= val <= cz or c0 <= val <= c9:
+                    right = val
+                    break
+                elif cA <= val <= cZ:
+                    right = val + 32
+                    break
+                j -= 1
+
+            if left == -1 or right == -1:
+                break
+
+            if left != right:
                 return False
+
             i += 1
             j -= 1
+
         return True
 
+
+# False
+print(Solution().isPalindrome("race a car"))
+
+# True
+print(Solution().isPalindrome("a."))
+
+# True
+print(Solution().isPalindrome(" "))
 
 # True
 print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
