89cc2c1b274887593ca16182205a4566edebf480
Lenic Rayleigh <Lenic@live.cn>
2022-12-17 22:11:28 +0800

    使用对撞指针完成题目：345
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- array/345.py
+++ array/345.py
@@ -3,15 +3,15 @@ from collections import deque
 
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        q, stack, ans, vowels = deque([]), deque([]), [], set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
-        for i, val in enumerate(s):
-            ans.append(val)
-            if val in vowels:
-                q.append(i)
-                stack.append(val)
-        while len(q):
-            ans[q.popleft()] = stack.pop()
-        return "".join(ans)
+        data, vowls = list(s), set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"])
+        i, j = 0, len(data) - 1
+        while i < j:
+            while i < j and data[i] not in vowls:
+                i += 1
+            while i < j and data[j] not in vowls:
+                j -= 1
+            data[i], data[j], i, j = data[j], data[i], i + 1, j - 1
+        return "".join(data)
 
 
 # holle
