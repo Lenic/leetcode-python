5ba6d5bcf42220b5531f2ba0919f4eb1f01d0c93
Lenic Rayleigh <Lenic@live.cn>
2022-12-21 23:01:45 +0800

    使用优化过的贪心算法解决题目：122
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- array/122.py
+++ array/122.py
@@ -3,14 +3,9 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans = prev = 0
+        ans: int = 0
         for i in range(1, len(prices)):
-            if prices[i - 1] > prices[i]:
-                ans += prices[i - 1] - prices[prev]
-                prev = i
-            elif i == len(prices) - 1:
-                if prices[i] > prices[prev]:
-                    ans += prices[i] - prices[prev]
+            ans += max(0, prices[i] - prices[i - 1])
         return ans
 
 
