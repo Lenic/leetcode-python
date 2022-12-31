e4b38cda59bd6c517f1ddf3f03b04a296bab577e
Lenic Rayleigh <Lenic@live.cn>
2023-01-01 02:18:49 +0800

    使用 KMP 算法完成题目：28
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- string/28.py
+++ string/28.py
@@ -1,18 +1,47 @@
+from typing import List
+
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         m, n = len(haystack), len(needle)
         if m < n:
             return -1
-        for i in range(m - n + 1):
-            j = 0
+
+        next = [0] * n
+        next[0] = -1
+        # build next
+        for i in range(1, n - 1):
+            right = i + 1
+            while right > 1:
+                right -= 1
+                j, k = 0, right
+                while k <= i:
+                    if needle[j] != needle[k]:
+                        break
+                    j, k = j + 1, k + 1
+                if k > i:
+                    next[i + 1] = j
+
+        i, j, count = 0, 0, m - n + 1
+        while i < count:
             while j < n:
                 if haystack[i + j] != needle[j]:
                     break
                 j += 1
             if j == n:
                 return i
+            else:
+                i, j = i + j - next[j], i + j
+                j = max(0, j - i)
+
         return -1
 
+
+# -1
+print(Solution().strStr(haystack="mississippi", needle="issipi"))
+
+# 15
+print(Solution().strStr(haystack="BBC ABCDAB ABCDABCDABDE", needle="ABCDABD"))
 
 # 2
 print(Solution().strStr(haystack="abc", needle="c"))
