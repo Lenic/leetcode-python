84ba923b87d16f8dfd20a02c854ad1c21baa615e
Lenic Rayleigh <Lenic@live.cn>
2023-01-04 22:09:34 +0800

    使用快慢指针完成题目：19
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- linked-list/19.py
+++ linked-list/19.py
@@ -1,50 +1,32 @@
 from typing import List, Optional
 from typing_extensions import Self
 
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x: int = 0, next: Self | None = None):
-        self.val = x
-        self.next = next
+from listNode import convertArray, convertLinkedList, ListNode
 
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if not head:
-            return None
-
-        slow = fast = dummy = ListNode(0, head)
+        if head is None:
+            return head
+        left = right = dummy = ListNode(0, head)
         for _ in range(n):
-            if fast:
-                fast = fast.next
+            if right:
+                right = right.next
             else:
-                break
-
-        while fast and fast.next:
-            fast = fast.next
-            node = slow.next
-            if node:
-                slow = node
-
-        if slow.next:
-            node = slow.next
-            if node:
-                slow.next = node.next
-
+                return head
+        while right and right.next:
+            right = right.next
+            if left:
+                left = left.next
+        if left and left.next:
+            left.next = left.next.next
         return dummy.next
 
 
 def polyfill(data: List[int], n: int):
-    cur = head = ListNode()
-    for val in data:
-        cur.next = ListNode(val)
-        cur = cur.next
-    res = Solution().removeNthFromEnd(head.next, n)
-    ans: List[int] = []
-    while res:
-        ans.append(res.val)
-        res = res.next
-    print(ans)
+    cur = convertArray(data)
+    res = Solution().removeNthFromEnd(cur, n)
+    print(convertLinkedList(res))
 
 
 # []
@@ -52,3 +34,6 @@ polyfill([1], 1)
 
 # [1,2,3,5]
 polyfill([1, 2, 3, 4, 5], 2)
+
+# [1]
+polyfill([1, 2], 1)
