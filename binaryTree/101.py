7d7b26a7d7c3888c6d69b16b3ceabb7da4397ba3
Lenic Rayleigh <Lenic@live.cn>
2023-01-11 23:10:49 +0800

    使用双队列完成题目：101
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- binaryTree/101.py
+++ binaryTree/101.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from queue import Queue
+from collections import deque
 
 from utils import convertArray, TreeNode
 
@@ -9,21 +9,22 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return True
-        q: Queue[TreeNode | None] = Queue()
-        q.put_nowait(root.left)
-        q.put_nowait(root.right)
-        while not q.empty():
-            n1, n2 = q.get_nowait(), q.get_nowait()
-            if n1 is None and n2 is None:
-                continue
-            if n1 is None or n2 is None:
+        left, right = deque([root.left]), deque([root.right])
+        while len(left):
+            if len(right) != len(left):
                 return False
-            if n1.val != n2.val:
-                return False
-            q.put_nowait(n1.left)
-            q.put_nowait(n2.right)
-            q.put_nowait(n1.right)
-            q.put_nowait(n2.left)
+            for _ in range(len(left)):
+                x = left.popleft()
+                y = right.popleft()
+                if x is None and y is None:
+                    continue
+                if x is not None and y is not None and x.val == y.val:
+                    left.append(x.left)
+                    left.append(x.right)
+                    right.append(y.right)
+                    right.append(y.left)
+                else:
+                    return False
         return True
 
 
@@ -31,11 +32,11 @@ def polyfill(root: List[int | None]):
     print(Solution().isSymmetric(convertArray(root)))
 
 
+# False
+polyfill(root=[1, 2, 2, None, 3, None, 3])
+
 # True
 polyfill(root=[4, -57, -57, None, 67, 67, None, None, -97, -97])
 
 # True
 polyfill(root=[1, 2, 2, 3, 4, 4, 3])
-
-# False
-polyfill(root=[1, 2, 2, None, 3, None, 3])
