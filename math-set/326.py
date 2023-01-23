28daa73334d63b159db634057d06fbbe70b4a647
Lenic Rayleigh <Lenic@live.cn>
2023-01-23 20:48:25 +0800

    使用余数的方式解决题目：326
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- math-set/326.py
+++ math-set/326.py
@@ -2,13 +2,9 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n <= 0:
             return False
-        for i in range(n):
-            val = 3**i
-            if val == n:
-                return True
-            elif val > n:
-                return False
-        return False
+        while n % 3 == 0:
+            n = n // 3
+        return True if n == 1 else False
 
 
 # True
