de0664f3c847241fc2d28a28eb6b7cb9e84cbcb2
Lenic Rayleigh <Lenic@live.cn>
2023-01-28 22:35:16 +0800

    使用预置的静态数组替代动态数组完成题目：118
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- array/118.py
+++ array/118.py
@@ -4,12 +4,11 @@ from typing import List
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         ans: List[List[int]] = [[1]]
-        for _ in range(numRows - 1):
-            cur, next = ans[-1], [1]
-            for i in range(1, len(cur)):
-                next.append(cur[i - 1] + cur[i])
-            next.append(1)
-            ans.append(next)
+        for i in range(2, numRows + 1):
+            prev, cur = ans[-1], [1] * i
+            for j in range(1, len(prev)):
+                cur[j] = prev[j - 1] + prev[j]
+            ans.append(cur)
         return ans
 
 
