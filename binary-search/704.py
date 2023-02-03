065d60ca340749caffe5246049efc5080c5661eb
Lenic Rayleigh <Lenic@live.cn>
2023-02-03 21:42:12 +0800

    使用二分查找完成题目：704
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- binary-search/704.py
+++ binary-search/704.py
@@ -6,10 +6,10 @@ class Solution:
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = left + ((right - left) >> 1)
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
             else:
                 return mid
         return -1
