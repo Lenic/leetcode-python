bea7c3520f2bdb71072c54060e1b80d992cb2634
Lenic Rayleigh <Lenic@live.cn>
2023-02-10 23:55:44 +0800

    使用优化过的滑动窗口完成题目：438
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- sliding-window/438.py
+++ sliding-window/438.py
@@ -1,26 +1,32 @@
 from typing import List
-from collections import Counter, defaultdict
 
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        ans, pattern, cur, left = [], Counter(p), defaultdict(int), 0
+        ans: List[int] = []
+        if len(s) < len(p):
+            return ans
 
-        def isEqual() -> bool:
-            for key in pattern:
-                if cur[key] != pattern[key]:
-                    return False
-            return True
+        initialValue, pattern, cur = ord("a"), [0] * 26, [0] * 26
+        for i in range(len(p)):
+            pattern[ord(p[i]) - initialValue] += 1
+            cur[ord(s[i]) - initialValue] += 1
 
-        for right, val in enumerate(s):
-            cur[val] += 1
-            if right - left + 1 > len(p):
-                cur[s[left]] -= 1
-                left += 1
-            if right - left + 1 == len(p) and isEqual():
-                ans.append(left)
+        if pattern == cur:
+            ans.append(0)
+
+        for i in range(len(s) - len(p)):
+            cur[ord(s[i]) - initialValue] -= 1
+            cur[ord(s[i + len(p)]) - initialValue] += 1
+
+            if pattern == cur:
+                ans.append(i + 1)
+
         return ans
 
+
+# []
+print(Solution().findAnagrams(s="aa", p="bb"))
 
 # [0, 1, 2]
 print(Solution().findAnagrams(s="abab", p="ab"))
