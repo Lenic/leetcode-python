93d16b4abd7ade9805903976b5ba76d2e7f672c6
Lenic Rayleigh <Lenic@live.cn>
2023-02-21 01:00:06 +0800

    使用滑动窗口和 Hash 的解法，不如 Set 高效：904
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- sliding-window/904.py
+++ sliding-window/904.py
@@ -1,24 +1,19 @@
 from typing import List
 
+from collections import defaultdict
+
 
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        left, ans, cache = 0, 0, set()
+        left, ans, cache = 0, 0, defaultdict(int)
         for right, val in enumerate(fruits):
-            if len(cache) < 2:
-                cache.add(val)
-            elif val not in cache:
-                cache.clear()
-                ans = max(ans, right - left)
-                for i in range(right, left - 1, -1):
-                    if len(cache) < 2:
-                        cache.add(fruits[i])
-                    elif len(cache) == 2 and fruits[i] in cache:
-                        continue
-                    else:
-                        left = i + 1
-                        break
-        ans = max(ans, len(fruits) - left)
+            cache[val] += 1
+            while len(cache) > 2:
+                cache[fruits[left]] -= 1
+                if cache[fruits[left]] == 0:
+                    cache.pop(fruits[left])
+                left += 1
+            ans = max(ans, right - left + 1)
         return ans
 
 
