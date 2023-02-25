dd215f4ee18da6fb01668e92bb4a70b4ea9db3a6
Lenic Rayleigh <Lenic@live.cn>
2023-02-25 21:54:19 +0800

    使用滑动窗口完成题目：930
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- sliding-window/930.py
+++ sliding-window/930.py
@@ -4,12 +4,22 @@ from collections import defaultdict
 
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        ans, s, cnt = 0, 0, defaultdict(int)
-        for val in nums:
-            cnt[s] += 1
-            s += val
-            ans += cnt[s - goal]
-        return ans
+        n = len(nums)
+        left1 = left2 = right = 0
+        sum1 = sum2 = 0
+        ret = 0
+        while right < n:
+            sum1 += nums[right]
+            while left1 <= right and sum1 > goal:
+                sum1 -= nums[left1]
+                left1 += 1
+            sum2 += nums[right]
+            while left2 <= right and sum2 >= goal:
+                sum2 -= nums[left2]
+                left2 += 1
+            ret += left2 - left1
+            right += 1
+        return ret
 
 
 # 4
