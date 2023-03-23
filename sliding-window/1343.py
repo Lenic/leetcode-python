d7185ee9adbd951345101505fcb8336d24dd7335
Lenic Rayleigh <Lenic@live.cn>
2023-03-23 22:42:44 +0800

    优化前缀和的计算：1343
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- sliding-window/1343.py
+++ sliding-window/1343.py
@@ -3,20 +3,17 @@ from typing import List
 
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        target = threshold * k
-
-        prefixSum = [0] * (len(arr) + 1)
-        for i, val in enumerate(arr):
-            prefixSum[i + 1] = prefixSum[i] + val
-
-        ans: int = 0
-        for i in range(len(arr) - k + 1):
-            val = prefixSum[i + k] - prefixSum[i]
-            if val >= target:
+        ans, target, prefixSum = 0, threshold * k, sum(arr[0 : k - 1])
+        for i in range(k - 1, len(arr)):
+            prefixSum += arr[i]
+            if prefixSum >= target:
                 ans += 1
-
+            prefixSum -= arr[i - k + 1]
         return ans
 
+
+# 5
+print(Solution().numOfSubarrays(arr=[1, 1, 1, 1, 1], k=1, threshold=0))
 
 # 6
 print(Solution().numOfSubarrays(arr=[11, 13, 17, 23, 29, 31, 7, 5, 2, 3], k=3, threshold=5))
